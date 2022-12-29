b7bca4491176e7ecf3e197d3fe286da9ed558395
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-12-29 23:43:05 +0530

    Updated

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-tsc > node dist/index.js > app.out.log 2> app.err.log < /dev/null & 
+node index.js > app.out.log 2> app.err.log < /dev/null & 
