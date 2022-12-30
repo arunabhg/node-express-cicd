fad8ca86806c30dbdfacbe8cf02c6312cf81e862
Arunabh Ghosh <arunabh.tech@gmail.com>
2022-12-30 19:26:30 +0530

    Updated to install node specific version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16.16.0
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/node-codedeploy"
