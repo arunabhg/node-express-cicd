6198b54931f1d46512a3632f23672f4975dccbb1
Arunabh Ghosh <arunabh.tech@gmail.com>
2023-01-26 14:12:06 +0530

    Updated version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-const version = "2.3333333333";
+const version = "2.3434343434";
 
 app.get("/", (req, res) => {
 	res.send(
